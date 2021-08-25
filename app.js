974e8c325a6facdf5b178a6a5fe4d7f6f669f7f3
Eugeniy Pridatko <4nework@gmail.com>
2021-08-25 17:41:14 +0300

    square inline-styles

---
+++ app.js
@@ -0,0 +1,51 @@
+// let btnOne = document.querySelector('.btn1');
+// let btnTwo = document.querySelector('.btn2');
+// let div = document.querySelectorAll('div');
+
+
+// btnOne.addEventListener('click', function (evt) {
+//     evt.preventDefault();
+//     for (let i = 0; i < div.length; i++) {
+
+//         div[i].classList.add('red');
+//     }
+
+// });
+
+// btnTwo.addEventListener('click', function (evt) {
+//     evt.preventDefault();
+//     for (let i = 0; i < div.length; i++) {
+
+//         div[i].classList.add('green');
+//     }
+// });
+
+// for (let i = 0; i < div.length; i++) {
+//     div[i].addEventListener('click', function (evt) {
+//         evt.preventDefault();
+
+//         div[i].classList.add('size');
+
+//     });
+// }
+
+
+
+
+
+// let container = document.querySelector('.container')
+// let div = document.querySelector('div');
+// let bgSetting = document.querySelector('.background-setting');
+// let sizeSetting = document.querySelector('.size-setting');
+// let pixels = document.querySelector('.pixels');
+
+
+// bgSetting.onchange = function () {
+//     div.style.backgroundColor = bgSetting.value;
+// };
+
+// sizeSetting.onchange = function () {
+//     pixels.textContent = sizeSetting.value;
+//     div.style.width = sizeSetting.value + 'px';
+//     div.style.height = sizeSetting.value + 'px'
+// }
